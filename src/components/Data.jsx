61709fb441bb2d89cb5d4b7507f0e7b52441ff9b
OmkarMisal14 <omkarmisal1404@gmail.com>
2025-06-04 19:54:43 +0530

    Changes

---
+++ src/components/Data.jsx
@@ -0,0 +1,198 @@
+import React from 'react'
+
+export const Data = [
+    {
+        id:1,
+        name1:'Adarsh Aai',
+        images:[
+            {
+        id: 1,
+        imgSrc: '/images/shahu-maharaj.png',
+        imgTitle: 'S2dhsdfjsadhfdjk',
+        imgData:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quo mollitia, vitae voluptate quibusdam cupiditate temporibus aliquam dolorem, natus tempore nihil voluptatum impedit molestias aspernatur. Quos consequuntur quo maxime inventore doloribus vitae, nesciunt voluptas at, sapiente error natus doloremque nobis ipsa id eligendi?',
+    },
+    {
+        id: 2,
+        imgSrc: '/image/8.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 3,
+        imgSrc: '/image/8.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 4,
+        imgSrc: '/image/11.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 5,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 6,
+        imgSrc: '/image/9.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 7,
+        imgSrc: '/images/shahu-maharaj.png',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 8,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 9,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 10,
+        imgSrc: '/image/11.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 11,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 12,
+        imgSrc: '/image/9.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 13,
+        imgSrc: '/image/7.png',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 14,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 15,
+        imgSrc: '/image/11.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 16,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 17,
+        imgSrc: '/image/9.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 18,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+        ]
+    },
+    {
+        id:2,
+        name1:'Blood Donation',
+        images:[
+             {
+        id: 15,
+        imgSrc: '/image/11.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 16,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 17,
+        imgSrc: '/image/9.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 18,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+     {
+        id: 9,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 10,
+        imgSrc: '/image/11.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 11,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 12,
+        imgSrc: '/image/9.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+     {
+        id: 2,
+        imgSrc: '/image/8.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 3,
+        imgSrc: '/image/8.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 4,
+        imgSrc: '/image/11.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+    {
+        id: 5,
+        imgSrc: '/image/10.jpg',
+        imgTitle: 'Image title',
+        imgData: 'This is image data which is showing on new page of the website.'
+    },
+        ]
+    }
+]
+
+export default Data
