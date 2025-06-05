227b4c060512ace6044e4dec36a5189105f75e09
ameykulkarni27 <ameykulkarni041@gmail.com>
2025-06-05 12:02:22 +0530

    director board

---
+++ src/components/data/DirectorBoard.js
@@ -0,0 +1,99 @@
+const directorText = {
+  en: {
+    title: 'Director Board',
+    members: [
+      {
+        id: 1,
+        name: 'Mr. Vitthalrao Bhauso Patil (Kakaji)',
+        designation: 'President',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 2,
+        name: 'Mr. Sachin Sambhajirao Patil (Anna)',
+        designation: 'President',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 3,
+        name: 'Mrs. Malushree Vitthalrao Patil (Didi)',
+        designation: 'Secretary',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 4,
+        name: 'Mr. Pralhad Bhauso Patil (Bappa)',
+        designation: 'Member',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 5,
+        name: 'Mr. Dhairyashil Rajaram Patil (Vikasbhaiyya)',
+        designation: 'Member',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 6,
+        name: 'Mr. Gaganraj Vitthalrao Patil (Bhaiyya)',
+        designation: 'Member',
+        img: '/image/9.jpg',
+      },
+      {
+        id: 7,
+        name: 'Mrs. Mayuri Udaysingh Ghorpade (Mai)',
+        designation: 'Member',
+        img: '/image/9.jpg',
+      },
+    ],
+  },
+
+  mr: {
+    title: 'संचालक मंडळ',
+    members: [
+      {
+        id: 1,
+        name: 'श्री.विठ्ठलराव भाऊसो पाटील (काकाजी)',
+        designation: 'अध्यक्ष',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 2,
+        name: 'श्री. सचीन संभाजीराव पाटील (आण्णा)',
+        designation: 'अध्यक्ष',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 3,
+        name: 'सौ. मालुश्री विठ्ठलराव पाटील (दिदी)',
+        designation: 'सचिव',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 4,
+        name: 'श्री. प्रल्हाद भाऊसो पाटील (बाप्पा)',
+        designation: 'सदस्य',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 5,
+        name: 'श्री. धैर्यशिल राजाराम पाटील (विकासभैय्या)',
+        designation: 'सदस्य',
+        img: '/image/6.jpg',
+      },
+      {
+        id: 6,
+        name: 'श्री. गगनराज विठ्ठलराव पाटील (भैय्या)',
+        designation: 'सदस्य',
+        img: '/image/9.jpg',
+      },
+      {
+        id: 7,
+        name: 'सौ. मयुरी उदयसिंह घोरपडे (माई)',
+        designation: 'सदस्य',
+        img: '/image/9.jpg',
+      },
+    ],
+  },
+};
+
+export default directorText;
